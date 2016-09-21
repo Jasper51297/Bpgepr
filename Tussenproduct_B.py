e870f34dc45d70a9b1a4191c257409446c9650c6
Jasper51297 <jaspervandalum@gmail.com>
2016-09-21 22:26:37 +0200

    Finished

--- Tussenproduct_B.py
+++ Tussenproduct_B.py
@@ -7,21 +7,23 @@ import os
 import sys
 
 def menu():
-    print('==========OPTIONS==========')
+    print('\n\n\n==========OPTIONS==========')
     print('1. Download Files')
     print('2. BLAST')
     print('3. E-value sort')
     print('4. Get Proteïns')
-    print('5. Alles')
-    print('6. Exit')
+    print('5. Print Results')
+    print('6. Alles')
+    print('7. Exit')
     m_input = input('Input: ')
     return str(m_input)
     
 def download():
-    #moet tabel van proteins downloadenhttp://www.ncbi.nlm.nih.gov/genomes/Genome2BE/genome2srv.cgi?action=GetFeatures4Grid&amp;download=on&amp;type=Proteins&amp;genome_id=3066&genome_assembly_id=34269&mode=2&is_locus=1&is_locus_tag=0&optcols=1,0,,0,0&amp;filterText=replicon.gi%20=%20-1
     cmd1 = 'wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF_000189315.1_Devil_ref_v7.0/GCF_000189315.1_Devil_ref_v7.0_protein.faa.gz'
     cmd2 = 'gunzip GCF_000189315.1_Devil_ref_v7.0_protein.faa'
     cmd3 = 'mv GCF_000189315.1_Devil_ref_v7.0_protein.faa TasDev.fa'
+    #Niet mogelijk om van deze site te downloaden, needs fix.
+    #cmd4 = 'wget http://www.ncbi.nlm.nih.gov/genomes/Genome2BE/genome2srv.cgi?action=GetFeatures4Grid&amp;download=on&amp;type=Proteins&amp;genome_id=3066&genome_assembly_id=34269&mode=2&is_locus=1&is_locus_tag=0&optcols=1,0,,0,0&amp;filterText=replicon.gi%20=%20-1'
     cmd = [cmd1, cmd2, cmd3]
     for i in cmd:
         os.system(i)
@@ -41,9 +43,16 @@ def TopHits():
     print('De files met een lage E-Value staan in E0.txt')
 
 def getProtein():
-    #moet greppen protein code uit protein tabel
-    os.system('cat TasDev.fa | egrep -f tophits.txt > test')
+    #moet greppen protein code uit protein tabel (ProteinTable.txt)
+    os.system('cat ProteinTable.txt | egrep -f tophits.txt | awk "{print $1}" > protein.txt')
+    os.system("""cat protein.txt | tr '\t' '!' | awk -F '!' '{print $2 "\t" $6 "\t" $7 "\t" $8 "\t" $10}' > results.txt""")
     print('De namen van de eiwitten met een lage E-Value staan in protein.txt')
+
+def printResults():
+    inFile = open('results.txt')
+    inhoud = inFile.read()
+    print('Replicon Acc \t GeneID \t Locus \t proteïn product \t Protein name ')
+    print(inhoud)
 
 def main():
     while 1 == 1:
@@ -57,11 +66,13 @@ def main():
         if m_input == '4':
             getProtein()
         if m_input == '5':
+            printResults()
+        if m_input == '6':
             download()
             BLAST()
             TopHits()
             getProtein()
-        if m_input == '6':
+        if m_input == '7':
             sys.exit()
             
 main()
