92240e7c8359064ff859966b0ce93359b2298ae0
Jasper51297 <jaspervandalum@gmail.com>
2016-10-25 22:58:54 +0200

    Added documentation to functions

--- script.py
+++ script.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python
 """
+Scriptname: script.py
 Author: Jasper van Dalum, Paul de Raadt, Brenda van den Berg, Duncan Wierenga, Joery de Vries
-Version: 3.0
+Date: 25/10/2016
+Version: 3.1
 """
-#GeneID (KEY) | Gennaam | Genstart | Genstop | AccessionCode | EiwitNaam | PathwayName | PathwayDesc | EC | geneSeq | protSeq | exonStart | exonStop | chromosome
-
 import os
 import psycopg2
 import sys
-from subprocess import call
+
 
 def menu():
+    """Deze functie weergeeft het menu en vraagt de gebruiker om input.
+
+    Returns:
+    - Input van de gebruiker
+    """
     print '\n\n\n==========OPTIONS=========='
     print '1. Download Files'
     print '2. BLAST'
@@ -19,25 +24,40 @@ def menu():
     print '5. Exit'
     m_input = input('Input: ')
     return str(m_input)
-    
+
+
 def download():
-    cmd1 = 'wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF_000189315.1_Devil_ref_v7.0/GCF_000189315.1_Devil_ref_v7.0_protein.faa.gz -O TasDev.fa.gz'
-    cmd2 = 'gunzip TasDev.fa'
-    cmd3 = 'wget "http://www.ncbi.nlm.nih.gov/genomes/Genome2BE/genome2srv.cgi?action=GetFeatures4Grid&amp;download=on&amp;type=Proteins&amp;genome_id=3066&genome_assembly_id=34269&mode=2&is_locus=1&is_locus_tag=0&optcols=1,0,,0,0&amp;filterText=replicon.gi%20=%20-1" -O ProteinTable.txt'
-    cmd4 = 'wget "ftp://ftp.ncbi.nlm.nih.gov/genomes/Sarcophilus_harrisii/GFF/ref_Devil_ref_v7.0_scaffolds.gff3.gz"'
-    cmd5 = 'gunzip ref_Devil_ref_v7.0_scaffolds.gff3'
-    cmd = [cmd1, cmd2, cmd3, cmd4, cmd5]
-    for i in cmd:
-        os.system(i)
+    """Deze functie download files om de gegevens uit te halen.
+    De file wordt geunzipt als de file een zip bestand is.
+    """
+    os.system('wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF_000189315.1_Devil_ref_v7.0/GCF_000189315.1_Devil_ref_v7.0_protein.faa.gz -O TasDev.fa.gz')
+    os.system('gunzip TasDev.fa')
+    os.system('wget "http://www.ncbi.nlm.nih.gov/genomes/Genome2BE/genome2srv.cgi?action=GetFeatures4Grid&amp;download=on&amp;type=Proteins&amp;genome_id=3066&genome_assembly_id=34269&mode=2&is_locus=1&is_locus_tag=0&optcols=1,0,,0,0&amp;filterText=replicon.gi%20=%20-1" -O ProteinTable.txt')
+    os.system('wget "ftp://ftp.ncbi.nlm.nih.gov/genomes/Sarcophilus_harrisii/GFF/ref_Devil_ref_v7.0_scaffolds.gff3.gz"')
+    os.system('gunzip ref_Devil_ref_v7.0_scaffolds.gff3')
+
 
 def BLAST():
-    cmd1 = 'formatdb -i TasDev.fa -p T -o'
-    cmd2 = 'blastall -p blastx -m 8 -d TasDev.fa -i sequenties_groep_04.fa -o out_blast.txt'
-    cmd = [cmd1, cmd2]
-    for i in cmd:
-        os.system(i)
+    """Deze functie blast de sequenties uit
+    sequenties_groep_04.fa tegen de file TasDev.fa
+    """
+    os.system('formatdb -i TasDev.fa -p T -o')
+    os.system('blastall -p blastx -m 8 -d TasDev.fa -i sequenties_groep_04.fa -o out_blast.txt')
+
 
 def getResults():
+    """Deze functie selecteerd de unieke eiwitten uit out_blast.txt.
+    Voor elk van deze eiwitten worden functies opgeroepen die resultaten
+    verzamelen over dit eiwit. De gevonden resultaten worden in een dictionary
+    gezet met een geneid als key, en een list met gennaam, genstart,
+    genstop, accessioncode, eiwitnaam, pathwaynaam, pathwayomschrijving,
+    EC nummer, gensequentie, proteinsequentie, exonstart, exonstop en
+    chromosoom als values. om een aantal gegevens te kunnen downloaden
+    zullen kegg pagina's voor elk genid worden ingelezen.
+
+    Returns:
+    - results (Dictionary met resultaten)
+    """
     os.system("sort -k1,1 -k12,12nr -k11,11n  out_blast.txt | sort -u -k1,1 --merge | tr '\t' '!'| awk -F '!' '{print $2}' > tophits.txt")
     results = getProtein()
     for geneid in results.keys():
@@ -52,8 +72,19 @@ def getResults():
             lst.append(var)
         results[geneid] = lst
     return results
-        
+
+
 def getProtein():
+    """Deze functie zoekt gegevens uit de file ProteinTable.txt
+    bij de eiwitten in de file tophits.txt. Alle resultaten
+    worden in een dictionary gestopt. De key is geneid en de
+    values zijn: gennaam, genstart, genstop, accessioncode en
+    eiwitnaam.
+
+    Returns:
+    - results
+    * Dictionary met resultaten uit ProteinTable.txt
+    """
     results = {}
     os.system('cat ProteinTable.txt | egrep -f tophits.txt | awk "{print $1}" > protein.txt')
     proteins = os.popen("""cat protein.txt | tr '\t' '!' | awk -F '!' '{print $6 "\t" $7 "\t" $3 "\t" $4 "\t" $8 "\t" $10}'""").readlines()
@@ -66,7 +97,19 @@ def getProtein():
         results[key] = line
     return results
 
+
 def getPathways():
+    """Deze functie opent de kegg file en haalt de
+    pathwaynamen en pathwayomschrijvingen eruit.
+
+    Returns:
+    - pathwayname
+    * Dit is een string met daarin alle pathwaynamen. Ze zijn gescheiden
+    door een \t.
+    - pathwaydesc
+    * Dit is een string met daarin alle pathwayomschrijvingen. Ze zijn gescheiden
+    door een \t.
+    """
     pathway = os.popen("cat kegg | tr -d '\n' | tr ')' '\n' | sed 's/Module/\\n/g' | sed 's/Brite/\\n/g' |egrep Pathway | sed 's/Pathway//g' | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'").read()
     pathways = pathway.split('   ')
     a, pathwayname, pathwaydesc = 0, '', ''
@@ -81,19 +124,55 @@ def getPathways():
     pathwaydesc, pathwayname = pathwaydesc.strip(), pathwayname.strip()
     return pathwayname, pathwaydesc
 
+
 def getEC():
-    EC = os.popen("cat kegg|egrep EC: | awk -F '[\[\]]' '{print $2}'").read()
+    """ Deze functie opent de kegg file en haalt de EC nummers eruit.
+
+    Returns:
+    - EC
+    * Dit is een string. EC nummers worden gescheiden met een \t.
+    """
+    EC = os.popen("cat kegg | egrep EC: | awk -F '[\[\]]' '{print $2}'").read()
     EC = EC.replace('EC:', '')
     EC = EC.replace('\n', '')
     EC = EC.replace(' ', '\t')
     return EC
 
+
 def getSeq(AccCode):
+    """Deze functie download de gensequentie en de proteine sequentie
+    die bij AccCode hoort.
+
+    Parameters:
+    - AccCode (accessioncode)
+
+    Returns:
+    - geneSeq
+    * Gensequentie (aminozuren)
+    - protSeq
+    * Eiwitsequentie (aminozzuren)
+    """
     geneSeq = os.popen("cat kegg | tr -d '\n' | sed 's/DB search/\\n!/g' | sed 's/NT seq/\\n/g' | egrep ! | tr -d ! | tr -d ' ' | tr -d '\n'").read()
     protSeq = os.popen("cat TasDev.fa | tr -d '\n' | tr '>' '\n' | egrep %s | sed 's/]/\\n!/g' | egrep ! | tr -d ! | tr -d '\n'"%AccCode).read()
     return geneSeq, protSeq
 
+
 def getExon(geneid):
+    """Deze functie haalt start en eindposities van het
+    gen op en het chromosoom waar het gen ligt.
+
+    Parameters:
+    - geneid
+    * genid
+
+    Returns:
+    - exonstart
+    * String met startposities van de exonen, gescheiden door een \t.
+    - exonstop
+    * String met stopposities van de exonen, gescheiden door een \t.
+    - chrom
+    * string met chromosoom waar het gen op ligt
+    """
     exons =  os.popen("cat ref_Devil_ref_v7.0_scaffolds.gff3 | egrep %s | egrep exon | awk '{print $4, $5}'"%geneid).readlines()
     exonStart, exonStop = '', ''
     for pos in exons:
@@ -104,11 +183,23 @@ def getExon(geneid):
         exonStop += poslist[1]
     exonStop = exonStop.strip()
     exonStart = exonStart.strip()
-    NW =  os.popen("cat ref_Devil_ref_v7.0_scaffolds.gff3 | egrep %s | tr '\t' '\n' | egrep NW_ | uniq | tr -d '\n'"%geneid).read()
+    NW = os.popen("cat ref_Devil_ref_v7.0_scaffolds.gff3 | egrep %s | tr '\t' '\n' | egrep NW_ | uniq | tr -d '\n'"%geneid).read()
     chrom = os.popen("cat ref_Devil_ref_v7.0_scaffolds.gff3 | egrep %s | tr ';' '\n' | egrep chromosome= | tr '=' '\n' |egrep -v chromosome | tr -d '\n'"%NW).read()
     return exonStart, exonStop, chrom
 
+
 def login():
+    """Deze functie laat Python connectie maken met de database
+    en zorgt ervoor dat autocommit aanstaat zodat alle veranderingen
+    worden opgeslagen.
+
+    Returns:
+    - cursor
+    * Met dit object kunnen queries worden uitgevoerd in de database
+    - conn
+    * Dit is het connection object, deze zal gebruikt worden om de
+    connection te sluiten.
+    """
     conn_string = "host='127.0.0.1' dbname='bpgepr' user='user' password='password'"
     print "Connecting to database\n	->%s" % (conn_string)
     conn = psycopg2.connect(conn_string)
@@ -117,30 +208,64 @@ def login():
     cursor = conn.cursor()
     return cursor, conn
 
+
 def deleteTables(cursor):
+    """Deze functie verwijderd tabellen in de database als ze bestaan.
+
+    Parameters:
+    - cursor
+    * Met dit object kunnen queries worden uitgevoerd in de database
+    """
     tablelist = ['EC_04', 'EIWIT_PATHWAY_04', 'PATHWAY_04', 'EXON_04', 'EIWIT_04', 'GEN_04']
     for table in tablelist:
         cursor.execute("DROP TABLE IF EXISTS %s"%table)
 
+
 def createTables(cursor):
+    """Deze functie maakt de tabellen aan, geeft aan welke attributen keys zijn
+    en welke restricties toegekend moeten worden.
+
+    Parameters:
+    - cursor
+    * Met dit object kunnen queries worden uitgevoerd in de database
+    """
     GEN_04 = "CREATE TABLE GEN_04 (GenID VARCHAR(25) NOT NULL UNIQUE PRIMARY KEY, Naam VARCHAR(25), Chromosoom VARCHAR(2), Start_gen INT, Stop_gen INT, Sequentie TEXT);"
     EXON_04 = "CREATE TABLE EXON_04 (GenID VARCHAR(25) NOT NULL REFERENCES GEN_04(GenID), Exon_start INT NOT NULL, Exon_stop INT NOT NULL, PRIMARY KEY(GenID, Exon_start));"
     EIWIT_04 = "CREATE TABLE EIWIT_04 (Accession_code VARCHAR(25) UNIQUE PRIMARY KEY, GenID VARCHAR(25) REFERENCES GEN_04(GenID), Naam TEXT, Sequentie TEXT);"
     EC_04 = "CREATE TABLE EC_04 (Accession_code VARCHAR(25) REFERENCES EIWIT_04(Accession_code), EC_nummer VARCHAR(25), PRIMARY KEY(Accession_code, EC_nummer));"
     PATHWAY_04 = "CREATE TABLE PATHWAY_04 (PathwayID VARCHAR(25) PRIMARY KEY, Omschrijving TEXT);"
-    EIWIT_PATHWAY_04 ="CREATE TABLE EIWIT_PATHWAY_04 (Accession_code VARCHAR(25) REFERENCES EIWIT_04(Accession_code), PathwayID VARCHAR(25) REFERENCES PATHWAY_04(PathwayID), PRIMARY KEY(Accession_code, PathwayID));"
+    EIWIT_PATHWAY_04 = "CREATE TABLE EIWIT_PATHWAY_04 (Accession_code VARCHAR(25) REFERENCES EIWIT_04(Accession_code), PathwayID VARCHAR(25) REFERENCES PATHWAY_04(PathwayID), PRIMARY KEY(Accession_code, PathwayID));"
     commands = [GEN_04, EXON_04, EIWIT_04, EC_04, PATHWAY_04, EIWIT_PATHWAY_04]
     for sql in commands:
         cursor.execute(sql)
 
+
 def insertGen(cursor, results):
-    #GeneID (KEY) | Gennaam | Genstart | Genstop | AccessionCode | EiwitNaam | PathwayName | PathwayDesc | EC | geneSeq | protSeq | exonStart | exonStop | chromosome
+    """Deze functie voert een deel van de gegevens uit de dictionary results
+    in voor de tabel GEN_04.
+
+    Parameters:
+    - cursor
+    * Met dit object kunnen queries worden uitgevoerd in de database
+    - results
+    * Dit is een dictionary met resultaten.
+    """
     for geneid in results.keys():
         lst = results[geneid]
-        data, sql = (geneid, lst[0] , lst[12], lst[1], lst[2], lst[8]), "INSERT INTO GEN_04 (GenID, Naam, Chromosoom, Start_gen, Stop_gen, Sequentie) VALUES (%s, %s, %s, %s, %s, %s);"
+        data, sql = (geneid, lst[0], lst[12], lst[1], lst[2], lst[8]), "INSERT INTO GEN_04 (GenID, Naam, Chromosoom, Start_gen, Stop_gen, Sequentie) VALUES (%s, %s, %s, %s, %s, %s);"
         cursor.execute(sql, data)
 
+
 def insertExon(cursor, results):
+    """Deze functie voert een deel van de gegevens uit de dictionary results
+    in voor de tabel EXON_04.
+
+    Parameters:
+    - cursor
+    * Met dit object kunnen queries worden uitgevoerd in de database
+    - results
+    * Dit is een dictionary met resultaten.
+    """
     for geneid in results.keys():
         lst = results[geneid]
         exon_start, exon_stop, a = lst[10], lst[11], 0
@@ -157,13 +282,33 @@ def insertExon(cursor, results):
                 b = 1
             a += 1
 
+
 def insertEiwit(cursor, results):
+    """Deze functie voert een deel van de gegevens uit de dictionary results
+    in voor de tabel EIWIT_04.
+
+    Parameters:
+    - cursor
+    * Met dit object kunnen queries worden uitgevoerd in de database
+    - results
+    * Dit is een dictionary met resultaten.
+    """
     for geneid in results.keys():
         lst = results[geneid]
         data, sql = (lst[3], geneid, lst[4], lst[9]), "INSERT INTO EIWIT_04 (Accession_code, GenID, Naam, Sequentie) VALUES (%s, %s, %s, %s);"
         cursor.execute(sql, data)
 
+
 def insertEC(cursor, results):
+    """Deze functie voert een deel van de gegevens uit de dictionary results
+    in voor de tabel insertE.
+
+    Parameters:
+    - cursor
+    * Met dit object kunnen queries worden uitgevoerd in de database
+    - results
+    * Dit is een dictionary met resultaten.
+    """
     for geneid in results.keys():
         lst, a = results[geneid], 0
         if '\t' in lst[7]:
@@ -178,7 +323,17 @@ def insertEC(cursor, results):
                 b = 1
             a += 1
 
+
 def insertPathway(cursor, results):
+    """Deze functie voert een deel van de gegevens uit de dictionary results
+    in voor de tabel Pathway_04.
+
+    Parameters:
+    - cursor
+    * Met dit object kunnen queries worden uitgevoerd in de database
+    - results
+    * Dit is een dictionary met resultaten.
+    """
     for geneid in results.keys():
         lst, a = results[geneid], 0
         if '\t' in lst[5]:
@@ -193,9 +348,19 @@ def insertPathway(cursor, results):
                 b = 1
             a += 1
 
+
 def insertEiwitPathway(cursor, results):
+    """Deze functie voert een deel van de gegevens uit de dictionary results
+    in voor de tabel EIWIT_PATHWAY_04.
+
+    Parameters:
+    - cursor
+    * Met dit object kunnen queries worden uitgevoerd in de database
+    - results
+    * Dit is een dictionary met resultaten.
+    """
     for geneid in results.keys():
-        lst, a = results[geneid], 0 
+        lst, a = results[geneid], 0
         if '\t' in lst[5]:
             a, pathwayname = 0, lst[5].split('\t')
         else:
@@ -204,8 +369,8 @@ def insertEiwitPathway(cursor, results):
             data, sql = (lst[3], pathwayname[a]), "INSERT INTO EIWIT_PATHWAY_04 (Accession_code, PathwayID) VALUES (%s, %s);"
             cursor.execute(sql, data)
             a += 1
-            
-        
+
+
 def main():
     while 1 == 1:
         m_input = menu()
@@ -224,7 +389,7 @@ def main():
             cursor, conn = login()
             deleteTables(cursor)
             createTables(cursor)
-            with open('Results.txt','r') as resdict:
+            with open('Results.txt', 'r') as resdict:
                 results = eval(resdict.read())
             insertGen(cursor, results)
             insertExon(cursor, results)
@@ -236,5 +401,5 @@ def main():
             conn.close()
         if m_input == '5':
             sys.exit()
-            
+
 main()
